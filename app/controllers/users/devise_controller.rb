1945a92977d64c4aae62851215c29dd55ae32b5d
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-05-08 22:27:10 +0100

    Tidy up devise controller

--- app/controllers/users/devise_controller.rb
+++ app/controllers/users/devise_controller.rb
@@ -1,15 +1,33 @@
 class Users::DeviseController < ApplicationController
   class Responder < ActionController::Responder
     def to_turbo_stream
-      controller.render(options.merge(formats: :html))
+      render_success
     rescue ActionView::MissingTemplate => e
       raise e if get?
 
-      if has_errors? && default_action
-        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      if can_render_error?
+        render_regular
       else
-        redirect_to navigation_location
+        redirect
       end
+    end
+
+    private
+
+    def render_success
+      controller.render(options.merge(formats: :html))
+    end
+
+    def can_render_error?
+      has_errors? && default_action
+    end
+
+    def render_regular
+      render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+    end
+
+    def redirect
+      redirect_to navigation_location
     end
   end
 
