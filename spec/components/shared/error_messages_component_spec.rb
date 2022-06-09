5bbb1aaea68cdf5543e2cbd04bfe0fd1c50eae8d
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-06-09 20:59:46 +0100

    Require rails helper

--- spec/components/shared/error_messages_component_spec.rb
+++ spec/components/shared/error_messages_component_spec.rb
@@ -1,5 +1,3 @@
-require "rails_helper"
-
 RSpec.describe Shared::ErrorMessagesComponent, type: :component do
   subject(:component) { described_class.new(object: user) }
 
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,5 +1,3 @@
-require "rails_helper"
-
 RSpec.describe "Users", js: true do
   include Devise::Test::IntegrationHelpers
 
