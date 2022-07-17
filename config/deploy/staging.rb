726668a43fd10a3a628839993ab65e5ac4412bfe
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-07-17 23:35:17 +0100

    Try another way to get staging key up

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,3 +5,5 @@ set :rails_env, :staging
 set :deploy_to, "/var/www/staging.finalfurlong"
 
 set :deploytag_time_format, "%Y.%m.%d-%H%M%S-utc"
+
+append :linked_files, "config/credentials/staging.key"
