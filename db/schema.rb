f8d4046a16efb7a8857dd89333967dc85bed7dbe
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-07-17 22:10:56 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,21 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_17_190341) do
   create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
-
   create_table "horses", force: :cascade do |t|
     t.string "name"
     t.string "gender", null: false, comment: "colt, filly, mare, stallion, gelding"
