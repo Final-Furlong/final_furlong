dc1e6baba89b8d02c25f51d21966fa2d87e5a955
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-07-26 20:13:25 +0100

    Working nav!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,14 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "bootstrap"
-import "./controllers"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+const navMenu = document.getElementById("sidebar-menu")
+const hideIcon = document.getElementById("icon-hide-sidenav")
+const showIcon = document.getElementById("icon-show-sidenav")
+
+navMenu.addEventListener("hide.bs.collapse", () => {
+  showIcon.classList.remove("d-none")
+  hideIcon.classList.add("d-none")
+})
+navMenu.addEventListener("show.bs.collapse", () => {
+  hideIcon.classList.remove("d-none")
+  showIcon.classList.add("d-none")
+})
