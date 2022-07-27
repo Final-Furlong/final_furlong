e1f0cb2d9ad37b015d2f6c05c951655e2911b110
Shanthi Pendleton <pendletons@users.noreply.github.com>
2022-07-27 23:36:29 +0100

    Mostly working styles and view components (#35)

---
+++ app/components/shared/breadcrumbs_component.rb
@@ -0,0 +1,37 @@
+module Shared
+  class BreadcrumbsComponent < ViewComponent::Base
+    def initialize(links: [], title: nil, classes: "")
+      @title = title
+      @links = links
+      @classes = classes
+      super
+    end
+
+    private
+
+    def styled_links
+      @styled_links ||= @links.map do |link|
+        link[:classes] = @classes
+        if link == @links.last
+          last_link_metadata(link)
+        else
+          link_metadata(link)
+        end
+      end
+    end
+
+    def link_metadata(link)
+      link[:breadcrumb_class] = "breadcrumb-item"
+      link[:aria_current] = false
+      link[:last] = false
+      link
+    end
+
+    def last_link_metadata(link)
+      link[:breadcrumb_class] = "breadcrumb-item active"
+      link[:aria_current] = "page"
+      link[:last] = true
+      link
+    end
+  end
+end
