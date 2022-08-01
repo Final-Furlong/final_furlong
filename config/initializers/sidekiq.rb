37f7302c62e9385d08b94861c1fa5715fb76123d
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-08-01 20:40:25 +0100

    Set proper redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://redis.example.com:7372/0", password: ENV.fetch("REDIS_PASSWORD", "") }
+  config.redis = if Rails.env.development?
+                   { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0") }
+                 else
+                   { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0"), password: ENV.fetch("REDIS_PASSWORD", "") }
+                 end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://redis.example.com:7372/0", password: ENV.fetch("REDIS_PASSWORD", "") }
+  config.redis = if Rails.env.development?
+                   { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0") }
+                 else
+                   { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0"), password: ENV.fetch("REDIS_PASSWORD", "") }
+                 end
 end
