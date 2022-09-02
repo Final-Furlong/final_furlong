903d25329b801d81ba84dc2c29e35cb10a74f9c4
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-09-02 09:42:58 +0100

    Update cypress setup

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,11 @@ module.exports = defineConfig({
   videosFolder: "tmp/cypress_videos",
   trashAssetsBeforeRuns: false,
   video: false,
+  fixturesFolder: "spec/cypress/fixtures",
 
   e2e: {
+    specPattern: "spec/cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "spec/cypress/support/integration.{js,jsx,ts,tsx}"
     // setupNodeEvents(on, config) {}
   }
 })
