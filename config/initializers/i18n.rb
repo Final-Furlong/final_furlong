e9327ccb1f2045ace9aba654edd6286647cce077
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2022-09-04 18:16:42 +0100

    Hook up en-GB => en i18n fallback

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,13 @@
+I18n::Debug.logger = Logger.new(Rails.root.join("log/i18n.log")) if Rails.env.development? && defined? I18n::Debug
+
+module I18n
+  module Tasks
+    module MissingKeys
+      def locale_key_missing?(locale, key)
+        # use fallbacks
+        !I18n.exists?(key, locale) && !external_key?(key, locale) && !ignore_key?(key, :missing)
+      end
+    end
+  end
+end
+
--- config/initializers/i18n_debug.rb
+++
@@ -1,2 +0,0 @@
-I18n::Debug.logger = Logger.new(Rails.root.join("log/i18n.log")) if Rails.env.development? && defined? I18n::Debug
-
