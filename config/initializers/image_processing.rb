1477eeca74f81145488fba71e0c4b5d5093939f0
Shanthi Pendleton <pendletons@users.noreply.github.com>
2024-07-17 12:06:09 +0100

    Use tempfiles for horse images rather than saving to disk (#76)

---
+++ config/initializers/image_processing.rb
@@ -0,0 +1,4 @@
+ImageProcessing::Vips.vips_cache_set_max 100 # up to 100 last operations
+ImageProcessing::Vips.vips_cache_set_max_mem 10 * 1024 * 1024 # up to 10MB of data
+ImageProcessing::Vips.vips_cache_set_max_files 100 # up to 100 tracked files
+
