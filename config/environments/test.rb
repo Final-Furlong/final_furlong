7e5714ffc3ce2b436a5b203d787410011976a14f
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-09-22 00:46:10 +0100

    Revert extra db for test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,8 +33,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.active_job.queue_adapter = :test
 
   config.action_mailer.perform_caching = false
 
