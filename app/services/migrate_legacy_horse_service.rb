1eeb4102c1bacf082cf9a61dbb6d3776dec7c52a
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-09-24 12:18:44 +0100

    Improve legacy migration service some more

--- app/services/migrate_legacy_horse_service.rb
+++ app/services/migrate_legacy_horse_service.rb
@@ -12,13 +12,13 @@ class MigrateLegacyHorseService # rubocop:disable Metrics/ClassLength
 
     horse.update!(
       age: calculate_age,
-      date_of_birth: from_game_date(legacy_horse.date_of_birth),
-      date_of_death: dead? ? from_game_date(legacy_horse.date_of_death) : nil,
+      date_of_birth: from_game_date(legacy_horse.DOB),
+      date_of_death: dead? ? from_game_date(legacy_horse.DOD) : nil,
       gender: pick_gender,
       name: legacy_horse.name.presence,
       status: pick_status,
-      created_at: from_game_date(legacy_horse.dob),
-      legacy_id: legacy_horse.id,
+      created_at: from_game_date(legacy_horse.DOB),
+      legacy_id: legacy_horse.ID,
       breeder_id: find_breeder,
       owner_id: find_owner,
       location_bred_id: find_location,
@@ -33,31 +33,31 @@ class MigrateLegacyHorseService # rubocop:disable Metrics/ClassLength
   private
 
   def find_sire
-    return unless legacy_horse.sire
+    return unless legacy_horse.Sire
 
-    Horses::Horse.where(legacy_id: legacy_horse.sire).pick(:id)
+    Horses::Horse.where(legacy_id: legacy_horse.Sire).pick(:id)
   end
 
   def find_dam
-    return unless legacy_horse.dam
+    return unless legacy_horse.Dam
 
-    Horses::Horse.where(legacy_id: legacy_horse.dam).pick(:id)
+    Horses::Horse.where(legacy_id: legacy_horse.Dam).pick(:id)
   end
 
   def find_location
-    locations[legacy_horse.loc_bred]
+    locations[legacy_horse.LocBred]
   end
 
   def find_breeder
-    return final_furlong unless legacy_horse.breeder
+    return final_furlong unless legacy_horse.Breeder
 
-    Account::Stable.where(legacy_id: legacy_horse.breeder).pick(:id) || final_furlong
+    Account::Stable.where(legacy_id: legacy_horse.Breeder).pick(:id) || final_furlong
   end
 
   def find_owner
-    return final_furlong unless legacy_horse.owner
+    return final_furlong unless legacy_horse.Owner
 
-    Account::Stable.where(legacy_id: legacy_horse.owner).pick(:id) || final_furlong
+    Account::Stable.where(legacy_id: legacy_horse.Owner).pick(:id) || final_furlong
   end
 
   def final_furlong
@@ -67,7 +67,7 @@ class MigrateLegacyHorseService # rubocop:disable Metrics/ClassLength
   end
 
   def pick_status # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize
-    case legacy_horse.status.to_i
+    case legacy_horse.Status.to_i
     when 1
       Horses::Horse.statuses[:broodmare]
     when 2
@@ -94,7 +94,7 @@ class MigrateLegacyHorseService # rubocop:disable Metrics/ClassLength
   end
 
   def pick_gender # rubocop:disable Metrics/MethodLength
-    case legacy_horse.gender
+    case legacy_horse.Gender
     when "C"
       Horses::Horse.genders[:colt]
     when "F"
@@ -106,25 +106,25 @@ class MigrateLegacyHorseService # rubocop:disable Metrics/ClassLength
     when "G"
       Horses::Horse.genders[:gelding]
     else
-      raise StandardError, "Unexpected gender: #{legacy_horse.gender}"
+      raise StandardError, "Unexpected gender: #{legacy_horse.Gender}"
     end
   end
 
   def dead?
-    return true if legacy_horse.status.to_i == 2
-    return false unless legacy_horse.date_of_death
+    return true if legacy_horse.Status.to_i == 2
+    return false unless legacy_horse.DOD
 
-    legacy_horse.date_of_death >= legacy_horse.date_of_birth
+    legacy_horse.DOD >= legacy_horse.DOB
   end
 
   def calculate_age
     max_date = if dead?
-      from_game_date(legacy_horse.date_of_death || legacy_horse.die)
+      from_game_date(legacy_horse.DOD || legacy_horse.Die)
     else
       Date.current
     end
 
-    max_date.year - from_game_date(legacy_horse.date_of_birth).year
+    max_date.year - from_game_date(legacy_horse.DOB).year
   end
 
   def from_game_date(value)
