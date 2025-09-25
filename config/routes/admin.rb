03dda1300224019961549d11a54839b35875005e
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-09-25 17:48:12 +0100

    Add pghero gem

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,6 +1,7 @@
 authenticate :user, lambda { |user| user.admin? } do
   mount Motor::Admin => "/motor_admin"
   mount MissionControl::Jobs::Engine, at: "/jobs"
+  mount PgHero::Engine, at: "/pghero"
 
   namespace :admin do
     resource :impersonate, only: %i[create destroy]
