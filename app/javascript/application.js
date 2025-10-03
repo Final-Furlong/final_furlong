98a0894f40e9838bf922614806af7abb29fae71a
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-10-03 22:52:52 +0100

    Update turbo progress bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,14 @@
 import "controllers"
 import "@hotwired/turbo-rails"
 
+// The default of 500ms is too long and
+// users can lose the causal link between clicking
+// a link and seeing the browser respond
+/* eslint-disable no-undef */
+if (typeof Turbo.config !== "undefined") {
+  Turbo.config.drive.progressBarDelay = 100
+}
+/* eslint-enable no-undef */
+
 // disable turbo temporarily
 // Turbo.session.drive = false
