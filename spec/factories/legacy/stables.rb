6d14052930ca914b7fa62b4b76ea7b1067ce7a7b
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-10-05 23:19:26 +0100

    Fix legacy stable factory

--- spec/factories/legacy/stables.rb
+++ spec/factories/legacy/stables.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :legacy_stable, class: "Legacy::Stable" do
+    id { Faker::Number.number(digits: 5) }
     availableBalance { 10_000 }
     date { Date.current }
     totalBalance { 20_000 }
