90c5246f1d3e98728a6796fbda3635fd06b3b40a
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-10-10 19:26:17 +0100

    Fix horse attribute reference

--- db/data/20251008210711_populate_horse_attributes.rb
+++ db/data/20251008210711_populate_horse_attributes.rb
@@ -2,7 +2,7 @@ class PopulateHorseAttributes < ActiveRecord::Migration[8.0]
   def up
     Horses::Horse.where("date_of_birth != date_of_death").find_each do |horse|
       horse_age = horse.date_of_death.present? ? horse.date_of_death.year - horse.date_of_birth.year : Date.current.year - horse.date_of_birth.year
-      horse.attributes.create!(age: horse_age)
+      horse.horse_attributes.create!(age: horse_age)
     end
   end
 
