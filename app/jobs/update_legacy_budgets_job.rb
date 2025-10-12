0a0acae4b630837d41bbb66ac20b51c5b7c4e09e
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-10-12 15:00:36 +0100

    Update settings for migrating budgets

--- app/jobs/update_legacy_budgets_job.rb
+++ app/jobs/update_legacy_budgets_job.rb
@@ -3,7 +3,7 @@ class UpdateLegacyBudgetsJob < ApplicationJob
 
   def perform
     budget_id = Account::Budget.maximum(:legacy_budget_id) || 0
-    MigrateLegacyBudgetsService.new(budget_id:).call
+    MigrateLegacyBudgetsService.new(budget_id:, limit: 1000).call
   end
 end
 
