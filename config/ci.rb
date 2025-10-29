46feaa7246c545a143ee899ed18ef838656641eb
Shanthi Pendleton <pendletons@users.noreply.github.com>
2025-10-29 13:51:13 +0000

    Update Github Workflows (#112)
    Update workflow to skip if local CI signed off

--- config/ci.rb
+++ config/ci.rb
@@ -1,32 +1,41 @@
 # Run using bin/ci
 
 CI.run do
-  if ENV.fetch("EXTERNAL_CI", false)
+  external_ci = ENV.fetch("EXTERNAL_CI", false)
+  if external_ci
     step "Setup", "bin/rails assets:precompile"
   else
     step "Setup", "bin/setup test"
   end
 
-  step "Style: Ruby", "bundle exec rubocop"
-  step "Style: Slim", "SLIM_LINT_RUBOCOP_CONF=.rubocop_slim.yml bundle exec slim-lint"
-  step "Style: ESLint", "yarn eslint app/javascript"
-  step "Style: StyleLint", "yarn run stylelint app/assets/tailwind"
-  step "Style: I18n", "bin/i18n-tasks health"
+  limited_ci = ENV.fetch("LIMITED_CI", false)
+  unless limited_ci
+    step "Style: Ruby", "bundle exec rubocop"
+    step "Style: Slim", "SLIM_LINT_RUBOCOP_CONF=.rubocop_slim.yml bundle exec slim-lint"
+    step "Style: ESLint", "yarn eslint app/javascript"
+    step "Style: StyleLint", "yarn run stylelint app/assets/tailwind"
+    step "Style: I18n", "bin/i18n-tasks health"
+    step "Style: Github Workflow", "actionlint"
+  end
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Security: Yarn vulnerability audit", "bin/yarn_audit.sh"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
-  step "Performance: Fasterer", "bin/fasterer"
-  step "Performance: Active Record Doctor", "bundle exec rake active_record_doctor"
+  unless limited_ci
+    step "Performance: Fasterer", "bin/fasterer"
+    step "Performance: Active Record Doctor", "bundle exec rake active_record_doctor"
+  end
 
-  step "Tests: Rails", "bin/rspec"
-  step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
+  unless limited_ci
+    step "Tests: Rails", "bin/rspec"
+    step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
+  end
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
-  unless ENV.fetch("EXTERNAL_CI", false)
+  unless external_ci || limited_ci
     if success?
       step "Signoff: All systems go. Ready for merge and deploy.", "gh signoff"
     else
