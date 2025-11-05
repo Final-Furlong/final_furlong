004a7ad33644819090419862973b253b551051a6
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-05 10:35:12 +0000

    Update stimulus notification js setup

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
+import Notification from "@stimulus-components/notification"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+Stimulus.register("notification", Notification)
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,13 @@
 # Pin npm packages by running ./bin/importmap
 pin "application", preload: true
 
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.3.0/app/javascript/turbo/index.js"
 pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js"
 pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.6/src/index.js"
 
-pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
 pin "stimulus-use" # @0.52.3
+pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
 
 pin_all_from "app/javascript/controllers", under: "controllers"
