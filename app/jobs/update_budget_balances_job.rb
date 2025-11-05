937939009b914744fde281e570f0eee8d61c5d28
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-05 17:24:37 +0000

    Add cursors to balance job

--- app/jobs/update_budget_balances_job.rb
+++ app/jobs/update_budget_balances_job.rb
@@ -4,15 +4,17 @@ class UpdateBudgetBalancesJob < ApplicationJob
   include ActiveJob::Continuable
 
   def perform
-    step :update_all_stables do |step|
-      Account::Stable.find_each(start: step.cursor) do |stable|
+    step :update_all_stables, start: [nil, nil] do |step|
+      Account::Stable.find_each(start: step.cursor[0]) do |stable|
         balance = 0
-        Account::Budget.where(stable:).order(created_at: :asc).find_each do |budget|
+        Account::Budget.where(stable:).order(created_at: :asc).find_each(start: step.cursor[1]) do |budget|
           balance += budget.amount
           budget.assign_attributes(balance:)
           budget.save(validate: false)
+          step.set! [stable.id, budget.id + 1]
         end
         stable.update(total_balance: balance)
+        step.set! [stable.id + 1, nil]
       end
     end
 
