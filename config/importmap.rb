16cbf367c82c2c88e6af64f6f10201ec6bceabf8
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-05 10:13:37 +0000

    Debug javascript

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,19 +1,13 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.1/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
 pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.3.0/app/javascript/turbo/index.js"
 pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js"
 pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.6/src/index.js"
 
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-
-pin_all_from "app/javascript/controllers", under: "controllers"
-
 pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
 pin "stimulus-use" # @0.52.3
 
+pin_all_from "app/javascript/controllers", under: "controllers"
