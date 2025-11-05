86a54b77bd499d1b8547ef26ea80b67a7ce155e8
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-05 11:52:48 +0000

    Update head + importmap to hopefully fix js issues

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,11 @@
 # Pin npm packages by running ./bin/importmap
-pin "application", preload: true
+pin "application"
 
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.3.0/app/javascript/turbo/index.js"
-pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js"
-pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.6/src/index.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin "@rails/activestorage", to: "activestorage.esm.js"
 
 pin "stimulus-use" # @0.52.3
 pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
