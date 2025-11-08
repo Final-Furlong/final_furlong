2d1b543343cd263e68e72797509d43dc1ab31055
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-08 10:41:43 +0000

    Override rubyopts for capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.0"
 
+ENV['RUBYOPT'] = ""
+
 server "ffdeploy", user: "www", roles: %i[web app db], primary: true
 
 set :application, "final_furlong"
@@ -13,7 +15,7 @@ set :rbenv_ruby, "3.4.5"
 
 append :linked_files, "config/database.yml", ".rbenv-vars"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system",
-  "vendor", "storage", ".bundle", "public/uploads", "public/vite"
+       "vendor", "storage", ".bundle", "public/uploads", "public/vite"
 append :assets_manifests, "public/vite/.vite/manifest*.*"
 
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
