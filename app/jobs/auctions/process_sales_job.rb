4c03f88e1d3e5942c4971b71492856fd6658dacf
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-15 15:04:46 +0000

    Remove concurrency from Auctions::ProcessSalesJob

--- app/jobs/auctions/process_sales_job.rb
+++ app/jobs/auctions/process_sales_job.rb
@@ -1,8 +1,6 @@
 class Auctions::ProcessSalesJob < ApplicationJob
   queue_as :default
 
-  limits_concurrency key: ->(bidder) { bidder }, duration: 5.minutes
-
   def perform(bidder, auction)
     Auctions::Bid.where(bidder:, auction:).current_high_bid.sale_time_met.each do |bid|
       Auctions::HorseSeller.new.process_sale(bid:)
