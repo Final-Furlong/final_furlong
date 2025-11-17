2fdc94bd74dcfd21be4bbe1c3deccbc9c75151a7
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-17 15:47:15 +0000

    Add job to set up recurring process sales jobs

--- app/jobs/auctions/process_sales_job.rb
+++ app/jobs/auctions/process_sales_job.rb
@@ -3,7 +3,10 @@ class Auctions::ProcessSalesJob < ApplicationJob
 
   def perform(bidder, auction)
     Auctions::Bid.where(bidder:, auction:).current_high_bid.sale_time_met.each do |bid|
-      Auctions::HorseSeller.new.process_sale(bid:)
+      winning_bid = Auctions::Bid.where(auction:, horse: bid.horse).sale_time_met.winning.first
+      if bid.id == winning_bid.id
+        Auctions::HorseSeller.new.process_sale(bid:)
+      end
     end
   end
 end
---
+++ app/jobs/auctions/schedule_sales_job.rb
@@ -0,0 +1,36 @@
+class Auctions::ScheduleSalesJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    Auction.active.find_each do |auction|
+      stable_ids = []
+      horses = auction.horses.where.associated(:bids).uniq
+      horses.each do |horse|
+        winning_bid = horse.bids.winning.first
+        stable_ids << winning_bid.bidder_id
+      end
+
+      stable_ids.each do |id|
+        unless job_exists?(auction.id, id)
+          times = []
+          Auctions::Bid.where(bidder: id, auction:).current_high_bid.each do |bid|
+            winning_bid = Auctions::Bid.where(auction:, horse: bid.horse).sale_time_met.winning.first
+            if bid == winning_bid
+              times << bid.updated_at
+            end
+          end
+          Auctions::ProcessSalesJob.set(wait_until: times.min).perform_later(bidder: id, auction:)
+        end
+      end
+    end
+  end
+
+  private
+
+  def job_exists?(auction_id, bidder_id)
+    SolidQueue::Job.where(class_name: "Auctions::ProcessSalesJob")
+      .where("arguments LIKE ?", "%#{bidder.id}%")
+      .exists?("arguments LIKE ?", "%#{auction.id}%")
+  end
+end
+
