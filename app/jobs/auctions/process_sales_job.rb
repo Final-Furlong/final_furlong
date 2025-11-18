4fc3c1cd2813651b215b585764fa105a26a80d80
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-18 10:37:04 +0000

    Refactor auction horse selling process

--- app/jobs/auctions/process_sales_job.rb
+++ app/jobs/auctions/process_sales_job.rb
@@ -1,9 +1,9 @@
 class Auctions::ProcessSalesJob < ApplicationJob
   queue_as :default
 
-  def perform(bidder, auction)
-    Auctions::Bid.joins(:horse).where(bidder:, auction:).sale_time_met.merge(Auctions::Horse.unsold).each do |bid|
-      winning_bid = Auctions::Bid.where(auction:, horse: bid.horse).sale_time_met.winning.first
+  def perform(auction)
+    Auctions::Bid.joins(:horse).where(auction:).sale_time_met.merge(Auctions::Horse.unsold).each do |bid|
+      winning_bid = Auctions::Bid.where(auction:, horse: bid.horse).sale_time_met.current_high_bid.first
       Auctions::HorseSeller.new.process_sale(bid: winning_bid)
     end
   end
