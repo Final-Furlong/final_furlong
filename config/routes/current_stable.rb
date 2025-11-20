40a81271f9f093dd476211c18b2757bd7b429b54
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-20 15:42:34 +0000

    Flesh out sales / sale offers

--- config/routes/current_stable.rb
+++ config/routes/current_stable.rb
@@ -3,7 +3,9 @@ resource :stable, only: %i[show edit update], controller: "stables", as: :curren
 namespace :stable, module: "current_stable" do
   resources :budgets, only: %i[index]
   resources :horses, only: %i[index edit show update]
+  resources :lease_offers, only: :index
   resources :leases, only: :index
+  resources :sale_offers, only: :index
   resources :training_schedules do
     resources :horses, controller: "training_schedule_horses", only: %i[index new create destroy]
   end
