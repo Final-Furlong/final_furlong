840dd83af68b893307c61ca48e1dbb17c215fb00
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-21 08:20:45 +0000

    Update Sentry info for new account

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,8 +2,8 @@ unless Rails.env.local?
   Sentry.init do |config|
     config.enabled_environments = %w[production]
 
-    config.dsn = "https://49bd65e43fd9495ba2c35e9d142cf5ed@o1325944.ingest.sentry.io/6585494"
-    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.dsn = "https://20f25e73cb4cac4892de8a7725b0a223@o4510391246651392.ingest.de.sentry.io/4510402025029712"
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
@@ -11,6 +11,10 @@ unless Rails.env.local?
     config.traces_sample_rate = 0.25
 
     config.rails.report_rescued_exceptions = false
+
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+    config.send_default_pii = true
   end
 end
 
