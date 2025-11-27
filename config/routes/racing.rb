9f1e7f8e73bd47c55619ea9f630479e0c166ab1c
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-11-27 21:58:13 +0000

    Display race results

--- config/routes/racing.rb
+++ config/routes/racing.rb
@@ -1,5 +1,7 @@
 namespace :racing do
   resources :races, only: :index
-  get "races/:date/:number", to: "races#show"
+  resources :results, only: :index
+  get "races/:date/:number", to: "races#show", as: :race
+  get "results/:date/:number", to: "results#show", as: :result
 end
 
