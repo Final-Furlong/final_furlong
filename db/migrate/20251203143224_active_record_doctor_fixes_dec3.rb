bb87467a42e9f3e41fd42ba9a48c9d33108c6832
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-12-03 16:05:56 +0000

    Remove invalid database migration

--- db/migrate/20251203143224_active_record_doctor_fixes_dec3.rb
+++
@@ -1,8 +0,0 @@
-class ActiveRecordDoctorFixesDec3 < ActiveRecord::Migration[8.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :race_entries, :odd_id, algorithm: :concurrently, if_not_exists: true
-  end
-end
-
