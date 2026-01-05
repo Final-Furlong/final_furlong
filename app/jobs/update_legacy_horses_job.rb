e424eee91d3cf98487e16983f686dff0863f4dbe
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-01-05 11:03:26 +0000

    Update legacy horse migration job to skip limit of 50 horses per run

--- app/jobs/update_legacy_horses_job.rb
+++ app/jobs/update_legacy_horses_job.rb
@@ -5,11 +5,11 @@ class UpdateLegacyHorsesJob < ApplicationJob
     if Racing::Racetrack.count < 21
       MigrateLegacyRacetrackService.new.call
     end
-    Legacy::Horse.where(rails_id: nil).limit(50).find_each do |legacy_horse|
+    Legacy::Horse.where(rails_id: nil).find_each do |legacy_horse|
       migrate_legacy_horse(legacy_horse:)
     end
     Legacy::Horse.where("last_modified > last_synced_to_rails_at")
-      .or(Legacy::Horse.where(last_synced_to_rails_at: nil)).limit(50).find_each do |legacy_horse|
+      .or(Legacy::Horse.where(last_synced_to_rails_at: nil)).find_each do |legacy_horse|
       migrate_legacy_horse(legacy_horse:)
     end
   end
