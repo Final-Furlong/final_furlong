e1a362b3cb250188b0774f6ff7e1e3383868f614
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-01-24 13:10:42 +0000

    Set up doing actual workouts

--- app/services/workouts/workout_comment_generator.rb
+++ app/services/workouts/workout_comment_generator.rb
@@ -1,5 +1,5 @@
 class Workouts::WorkoutCommentGenerator
-  attr_reader :knowledge, :stat, :value, :random_chance
+  attr_reader :knowledge, :stat, :value, :random_chance, :workout
 
   def initialize(knowledge:, stat:, value:, workout:)
     @knowledge = knowledge.clamp(10, 90)
@@ -12,235 +12,283 @@ class Workouts::WorkoutCommentGenerator
   def call
     comment_i18n_key = case stat.to_s.downcase
     when "equipment"
-      case value
-      when Racing::EquipmentStatusGenerator::HATE
-        if correct?
-          "equipment_20"
-        elsif totally_wrong?
-          "equipment_100"
-        else
-          "equipment_50"
-        end
-      when Racing::EquipmentStatusGenerator::LOVE
-        if correct?
-          "equipment_100"
-        elsif totally_wrong?
-          "equipment_20"
-        else
-          "equipment_50"
-        end
-      else
-        if correct?
-          "equipment_50"
-        elsif totally_wrong?
-          "equipment_100"
-        else
-          "equipment_20"
-        end
-      end
+      equipment_value
     when "stamina"
-      if total_distance > value
-        correct? ? "stamina_50" : "stamina_100"
-      else
-        correct? ? "stamina_100" : "stamina_50"
-      end
+      stamina_value
     when "energy"
-      if value < 51
-        correct? ? "energy_50" : "energy_100"
-      else
-        correct? ? "energy_100" : "energy_50"
-      end
+      energy_value
     when "fitness"
-      if value < 61
-        correct? ? "fitness_50" : "fitness_100"
-      else
-        correct? ? "fitness_100" : "fitness_50"
-      end
+      fitness_value
     when "happy"
-      if value < 21
-        "happy_20"
-      elsif value < 41
-        "happy_40"
-      elsif value < 61
-        "happy_60"
-      else
-        "happy_80"
-      end
+      happy_value
     when "weight"
-      if jockey_weight > value
-        "weight_100"
-      else
-        "weight_50"
-      end
+      weight_value
     when "style"
-      case value
-      when "leading"
-        if correct?
-          "style_20"
-        elsif mostly_wrong?
-          "style_80"
-        elsif mostly_right?
-          "style_60"
-        else
-          "style_40"
-        end
-      when "off_pace"
-        if correct?
-          "style_40"
-        elsif mostly_wrong?
-          "style_80"
-        elsif mostly_right?
-          "style_60"
-        else
-          "style_20"
-        end
-      when "midpack"
-        if correct?
-          "style_60"
-        elsif mostly_wrong?
-          "style_20"
-        elsif mostly_right?
-          "style_40"
-        else
-          "style_80"
-        end
-      else
-        if correct?
-          "style_80"
-        elsif mostly_wrong?
-          "style_20"
-        elsif mostly_right?
-          "style_40"
-        else
-          "style_60"
-        end
-      end
+      style_value
     when "pissy"
-      case value
-      when 1
-        if correct?
-          "pissy_20"
-        elsif totally_wrong?
-          "pissy_50"
-        else
-          "pissy_100"
-        end
-      when 2
-        if correct?
-          "pissy_50"
-        elsif totally_wrong?
-          "pissy_100"
-        else
-          "pissy_20"
-        end
-      when 3
-        if correct?
-          "pissy_50"
-        elsif totally_wrong?
-          "pissy_20"
-        else
-          "pissy_100"
-        end
-      when 4
-        if correct?
-          "pissy_50"
-        elsif totally_wrong?
-          "pissy_20"
-        else
-          "pissy_100"
-        end
-      when 5
-        if correct?
-          "pissy_100"
-        elsif totally_wrong?
-          "pissy_50"
-        else
-          "pissy_20"
-        end
-      end
+      pissy_value
     when "ratability"
-      case value
-      when 1
-        if correct?
-          "ratability_20"
-        elsif totally_wrong?
-          "ratability_50"
-        else
-          "ratability_100"
-        end
-      when 2
-        if correct?
-          "ratability_50"
-        elsif totally_wrong?
-          "ratability_100"
-        else
-          "ratability_20"
-        end
-      when 3
-        if correct?
-          "ratability_50"
-        elsif totally_wrong?
-          "ratability_20"
-        else
-          "ratability_100"
-        end
-      when 4
-        if correct?
-          "ratability_50"
-        elsif totally_wrong?
-          "ratability_20"
-        else
-          "ratability_100"
-        end
-      when 5
-        if correct?
-          "ratability_100"
-        elsif totally_wrong?
-          "ratability_50"
-        else
-          "ratability_20"
-        end
-      end
+      ratability_value
     when "xp"
-      if value < 11
-        "xp_10"
-      elsif value < 31
-        "xp_30"
-      elsif value < 41
-        "xp_40"
-      elsif value < 61
-        "xp_60"
-      elsif value < 71
-        "xp_70"
-      else
-        "xp_90"
-      end
+      xp_value
     when "confidence"
-      if value < 21
-        "confidence_20"
-      elsif value < 41
-        "confidence_40"
-      elsif value < 61
-        "confidence_60"
-      else
-        "confidence_80"
-      end
+      confidence_value
     when "natural_energy"
-      if value >= 80
-        "natural_energy_80"
-      elsif value >= 60
-        "natural_energy_60"
-      elsif value >= 40
-        "natural_energy_40"
-      elsif value >= 20
-        "natural_energy_20"
-      elsif value >= 0
-        "natural_energy_0"
-      else
-        "natural_energy_negative"
-      end
+      natural_energy_value
     end
     Racing::WorkoutComment.find_by(comment_i18n_key:)
+  end
+
+  def equipment_key
+    case value
+    when Racing::EquipmentStatusGenerator::HATE
+      if correct?
+        "equipment_20"
+      elsif totally_wrong?
+        "equipment_100"
+      else
+        "equipment_50"
+      end
+    when Racing::EquipmentStatusGenerator::LOVE
+      if correct?
+        "equipment_100"
+      elsif totally_wrong?
+        "equipment_20"
+      else
+        "equipment_50"
+      end
+    else
+      if correct?
+        "equipment_50"
+      elsif totally_wrong?
+        "equipment_100"
+      else
+        "equipment_20"
+      end
+    end
+  end
+
+  def stamina_value
+    if total_distance > value
+      correct? ? "stamina_50" : "stamina_100"
+    else
+      correct? ? "stamina_100" : "stamina_50"
+    end
+  end
+
+  def energy_value
+    if value < 51
+      correct? ? "energy_50" : "energy_100"
+    else
+      correct? ? "energy_100" : "energy_50"
+    end
+  end
+
+  def fitness_value
+    if value < 61
+      correct? ? "fitness_50" : "fitness_100"
+    else
+      correct? ? "fitness_100" : "fitness_50"
+    end
+  end
+
+  def happy_value
+    if value < 21
+      "happy_20"
+    elsif value < 41
+      "happy_40"
+    elsif value < 61
+      "happy_60"
+    else
+      "happy_80"
+    end
+  end
+
+  def weight_value
+    if jockey_weight > value
+      "weight_100"
+    else
+      "weight_50"
+    end
+  end
+
+  def style_value
+    case value
+    when "leading"
+      if correct?
+        "style_20"
+      elsif mostly_wrong?
+        "style_80"
+      elsif mostly_right?
+        "style_60"
+      else
+        "style_40"
+      end
+    when "off_pace"
+      if correct?
+        "style_40"
+      elsif mostly_wrong?
+        "style_80"
+      elsif mostly_right?
+        "style_60"
+      else
+        "style_20"
+      end
+    when "midpack"
+      if correct?
+        "style_60"
+      elsif mostly_wrong?
+        "style_20"
+      elsif mostly_right?
+        "style_40"
+      else
+        "style_80"
+      end
+    else
+      if correct?
+        "style_80"
+      elsif mostly_wrong?
+        "style_20"
+      elsif mostly_right?
+        "style_40"
+      else
+        "style_60"
+      end
+    end
+  end
+
+  def pissy_value
+    case value
+    when 1
+      if correct?
+        "pissy_20"
+      elsif totally_wrong?
+        "pissy_50"
+      else
+        "pissy_100"
+      end
+    when 2
+      if correct?
+        "pissy_50"
+      elsif totally_wrong?
+        "pissy_100"
+      else
+        "pissy_20"
+      end
+    when 3
+      if correct?
+        "pissy_50"
+      elsif totally_wrong?
+        "pissy_20"
+      else
+        "pissy_100"
+      end
+    when 4
+      if correct?
+        "pissy_50"
+      elsif totally_wrong?
+        "pissy_20"
+      else
+        "pissy_100"
+      end
+    when 5
+      if correct?
+        "pissy_100"
+      elsif totally_wrong?
+        "pissy_50"
+      else
+        "pissy_20"
+      end
+    end
+  end
+
+  def ratability_value
+    case value
+    when 1
+      if correct?
+        "ratability_20"
+      elsif totally_wrong?
+        "ratability_50"
+      else
+        "ratability_100"
+      end
+    when 2
+      if correct?
+        "ratability_50"
+      elsif totally_wrong?
+        "ratability_100"
+      else
+        "ratability_20"
+      end
+    when 3
+      if correct?
+        "ratability_50"
+      elsif totally_wrong?
+        "ratability_20"
+      else
+        "ratability_100"
+      end
+    when 4
+      if correct?
+        "ratability_50"
+      elsif totally_wrong?
+        "ratability_20"
+      else
+        "ratability_100"
+      end
+    when 5
+      if correct?
+        "ratability_100"
+      elsif totally_wrong?
+        "ratability_50"
+      else
+        "ratability_20"
+      end
+    end
+  end
+
+  def xp_value
+    if value < 11
+      "xp_10"
+    elsif value < 31
+      "xp_30"
+    elsif value < 41
+      "xp_40"
+    elsif value < 61
+      "xp_60"
+    elsif value < 71
+      "xp_70"
+    else
+      "xp_90"
+    end
+  end
+
+  def confidence_value
+    if value < 21
+      "confidence_20"
+    elsif value < 41
+      "confidence_40"
+    elsif value < 61
+      "confidence_60"
+    else
+      "confidence_80"
+    end
+  end
+
+  def natural_energy_value
+    if value >= 80
+      "natural_energy_80"
+    elsif value >= 60
+      "natural_energy_60"
+    elsif value >= 40
+      "natural_energy_40"
+    elsif value >= 20
+      "natural_energy_20"
+    elsif value >= 0
+      "natural_energy_0"
+    else
+      "natural_energy_negative"
+    end
   end
 
   def correct?
