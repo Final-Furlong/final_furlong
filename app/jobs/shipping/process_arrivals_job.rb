dd570f30fb4dcc18644f642777fb14875b465fb6
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-04-07 12:58:09 +0100

    Fix shipping arrivals job to do legacy db

--- app/jobs/shipping/process_arrivals_job.rb
+++ app/jobs/shipping/process_arrivals_job.rb
@@ -20,6 +20,14 @@ class Shipping::ProcessArrivalsJob < ApplicationJob
         racetrack:,
         at_home: shipment.shipping_type == "track_to_farm"
       )
+      Legacy::Horse.where(ID: horse.legacy_id).update(InTransit: 0)
+      ::Legacy::ViewRacehorses.find_by(horse_id: horse.legacy_id)&.update(in_transit: 0)
+      horses += 1
+    end
+    Shipping::BroodmareShipment.where("arrival_date > ? AND arrival_date <= ?", min_date, Date.current).find_each do |shipment|
+      horse = shipment.horse
+
+      Legacy::Horse.where(ID: horse.legacy_id).update(InTransit: 0)
       horses += 1
     end
     store_job_info(outcome: { horses:, date: min_date })
