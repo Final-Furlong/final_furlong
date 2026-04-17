cdf8359ec28e5042e48011001505be2d8ed8a9fe
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-04-17 20:14:02 +0100

    Toggle side menu

--- spec/support/shared/accessibility_examples.rb
+++ spec/support/shared/accessibility_examples.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples "a page that is accessible" do
   it "is axe clean", :axe do
     visit path_to_visit
-    expect(page).to be_axe_clean.excluding "#sidebar-toggle"
+    expect(page).to be_axe_clean.excluding "#menu-drawer"
   end
 end
 
