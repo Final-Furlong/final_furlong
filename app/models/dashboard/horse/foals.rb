354f2512a3f5687093338b33a07187d25373dd85
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-04-21 11:05:21 +0100

    Fix foals queries

--- app/models/dashboard/horse/foals.rb
+++ app/models/dashboard/horse/foals.rb
@@ -7,14 +7,14 @@ module Dashboard
       def initialize(horse:)
         @horse = horse
         if horse.female?
-          @born_foals = horse.foals.born.includes(:sire, :horse_attributes).order(date_of_birth: :asc)
-          @unborn_foal = horse.next_foal&.includes(:stud)
+          @born_foals = horse.foals.born.includes(:horse_attributes, :lifetime_race_record, sire: :lifetime_race_record).order(date_of_birth: :asc)
+          @unborn_foal = horse.next_foal
           @breeding_record = horse.broodmare_foal_record || horse.build_broodmare_foal_record
         else
           @breeding_record = horse.stud_foal_record || horse.build_stud_foal_record
           @crops_count = breeding_record.crops_count
           @crops_list = horse.stud_foals.group("DATE_PART('Year', date_of_birth)").count.sort
-          @born_foals = horse.stud_foals.born.includes(:dam, :horse_attributes).order(date_of_birth: :asc)
+          @born_foals = horse.stud_foals.born.includes(:horse_attributes, :lifetime_race_record, dam: :lifetime_race_record).order(date_of_birth: :asc)
           @unborn_foals = horse.breedings.bred.by_year(Date.current.year).includes(:mare).order(mare: { name: :asc })
         end
       end
