849f3dd2f814ee67f767c8be7b697d08f1a5fa1d
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-04-22 12:33:22 +0100

    Misc updates

--- app/services/horses/auto_namer.rb
+++ app/services/horses/auto_namer.rb
@@ -48,3 +48,4 @@ module Horses
     end
   end
 end
+
--- app/services/horses/status_updater.rb
+++ app/services/horses/status_updater.rb
@@ -1,24 +1,25 @@
 module Horses
   class StatusUpdater
     attr_reader :horse, :status
+
     def update_status(horse:, status:)
       @horse = horse
-      @status = Horses::Status::ACTIVE_BREEDING_STATUSES.include?(horse.status) ? "retired_#{horse.status}" : status_params[:status]
+      @status = Horses::Status::ACTIVE_BREEDING_STATUSES.include?(horse.status) ? "retired_#{horse.status}" : status
       result = Result.new(updated: false)
 
       ActiveRecord::Base.transaction do
         if horse.racehorse?
-          Horses::BoardingUpdater.new.stop_boarding(boarding: horse.current_boarding) if horse.current_boarding
+          BoardingUpdater.new.stop_boarding(boarding: horse.current_boarding) if horse.current_boarding
           horse.race_entries.delete_all
           horse.future_race_entries.delete_all
-          Racing::Claim.where(horse:).delete_all
+          ::Racing::Claim.joins(:entry).where(entry: { horse: }).delete_all
           # TODO: delete nominations
           horse.current_injuries.delete_all
-          horse.training_schedule_horses&.destroy
+          horse.training_schedules_horse&.destroy
         elsif horse.stud?
           horse.stud_options&.destroy
         end
-        @horse.update(status: new_status)
+        @horse.update(status:)
       end
       result.updated = @horse.valid?
       result
