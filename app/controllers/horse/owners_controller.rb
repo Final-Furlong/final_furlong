5463ca898463578bc9aa8d78c312b849e2cf986e
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-04-27 22:43:16 +0100

    Fix FF ownership

--- app/controllers/horse/owners_controller.rb
+++ app/controllers/horse/owners_controller.rb
@@ -2,6 +2,7 @@ module Horse
   class OwnersController < AuthenticatedController
     def update
       horse = Horses::Horse.managed_by(Current.stable).find(params[:id])
+      authorize horse, :give_to_game?, policy_class: CurrentStable::HorsePolicy
 
       result = Horses::OwnerUpdater.new.transfer_to_game(horse:, stable: Current.stable)
       if result.saved?
