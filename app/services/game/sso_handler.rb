92e0981440a5f8cc51f3dc7171ee808303a0979d
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-05-04 21:44:03 +0100

    Set up Discourse SSO

---
+++ app/services/game/sso_handler.rb
@@ -0,0 +1,50 @@
+module Game
+  class SSOHandler
+    attr_reader :return_url, :base64_payload
+
+    def initialize(return_url: nil)
+      @return_url = return_url
+    end
+
+    def nonce
+      @nonce ||= SecureRandom.hex
+    end
+
+    def sso_url_encoded
+      return @url_encoded if defined?(@url_encoded)
+
+      payload = "nonce=#{nonce}&return_sso_url=#{return_url}"
+      @base64_payload = encode_value(payload)
+      @url_encoded = CGI.escape(base64_payload)
+    end
+
+    def sig_hex_encoded
+      @hex_signature ||= hex_digest(base64_payload)
+    end
+
+    def sso_redirect_url
+      [
+        Rails.application.credentials.dig(:forum, :url),
+        Rails.application.credentials.dig(:forum, :sso_session_path).gsub(":sso", sso_url_encoded).gsub(":sig", sig_hex_encoded)
+      ].join("")
+    end
+
+    def hex_digest(value)
+      OpenSSL::HMAC.hexdigest("SHA256", Rails.application.credentials.dig(:forum, :sso_key), value).downcase
+    end
+
+    def base64?(data)
+      !(data =~ /[^a-zA-Z0-9=\r\n\/+]/m)
+    end
+
+    def encode_value(value)
+      Base64.encode64(value)
+    end
+
+    def decoded_hash(value)
+      hash = Rack::Utils.parse_query(Base64.decode64(value))
+      hash.symbolize_keys!
+    end
+  end
+end
+
--- spec/features/authentication/login_spec.rb
+++ spec/features/authentication/login_spec.rb
@@ -1,70 +1,23 @@
 RSpec.describe "Login Spec" do
-  it "allows login with email" do
-    admin = create(:admin)
+  context "when SSO works" do
+    it "allows login with email" do
+      admin
 
-    visit root_path
-    within("#top_nav") do
-      click_on t("layouts.nav.login")
+      visit root_path
+      within("#top_nav") do
+        click_on t("layouts.nav.login")
+      end
+      expect(page).to have_current_path current_stable_path
+      within(".badge") do
+        expect(page).to have_text t("view_components.users.online_badge.online")
+      end
+      expect(page.driver.request.cookies.keys).to include "_final_furlong_session"
     end
-    expect(page).to have_current_path new_user_session_path, ignore_query: true
-    fill_in "user[login]", with: admin.email
-    fill_in "user[password]", with: "abc1A$ab"
-    click_on t("devise.sessions.new.sign_in")
-
-    expect(page).to have_text t("devise.sessions.signed_in")
-    expect(page).to have_current_path root_path, ignore_query: true
-    visit current_stable_path
-    within(".badge") do
-      expect(page).to have_text t("view_components.users.online_badge.online")
-    end
-    expect(page.driver.request.cookies.keys).to include "_final_furlong_session"
   end
 
-  it "allows login with username" do
-    user = create(:user)
+  private
 
-    visit new_user_session_path
-    fill_in "user[login]", with: user.username
-    fill_in "user[password]", with: "abc1A$ab"
-    click_on t("devise.sessions.new.sign_in")
-
-    expect(page).to have_text t("devise.sessions.signed_in")
-    visit current_stable_path
-    within(".badge") do
-      expect(page).to have_text t("view_components.users.online_badge.online")
-    end
-    expect(page.driver.request.cookies.keys).to include "_final_furlong_session"
-  end
-
-  it "supports remember me" do
-    user = create(:user)
-
-    visit new_user_session_path
-    fill_in "user[login]", with: user.username
-    fill_in "user[password]", with: "abc1A$ab"
-    check "user_remember_me"
-    click_on t("devise.sessions.new.sign_in")
-    expect(page).to have_text t("devise.sessions.signed_in")
-    expect(page.driver.request.cookies.keys).to include "remember_user_token"
-  end
-
-  it "does not log in with empty values" do
-    visit new_user_session_path
-    click_on t("devise.sessions.new.sign_in")
-    expect(page).to have_current_path new_user_session_path, ignore_query: true
-  end
-
-  it "does not log in with invalid values" do
-    visit new_user_session_path
-    fill_in "user[login]", with: SecureRandom.alphanumeric(10)
-    fill_in "user[password]", with: SecureRandom.alphanumeric(10)
-    click_on t("devise.sessions.new.sign_in")
-    expect(page).to have_text t("devise.failure.invalid", authentication_keys: "login")
-    expect(page).to have_current_path new_user_session_path, ignore_query: true
-  end
-
-  it_behaves_like "a page that is accessible" do
-    let(:path_to_visit) { new_user_session_path }
+  def admin
+    @admin ||= create(:admin, email: "admin@example.com")
   end
 end
-
