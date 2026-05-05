f23eb7a325039c03cbb23229565bc111f2a84c22
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-05-05 13:52:32 +0100

    Show unbred mares on stable page

--- app/controllers/current_stable/unbred_mares_controller.rb
+++ app/controllers/current_stable/unbred_mares_controller.rb
@@ -4,7 +4,7 @@ module CurrentStable
       @query = policy_scope(Horses::Horse.broodmare.alive.where.missing(:next_foal), policy_scope_class: CurrentStable::HorsePolicy::Scope)
       @query = @query.order(name: :asc)
 
-      @pagy, @mares = pagy(:countless, @query)
+      @mares = @query.to_a
     end
   end
 end
