4789f9fda1b21d1382f662cf6e3eb0a04d09c5e5
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-05-21 12:18:52 +0100

    Set up babies to grow

---
+++ app/controllers/horse/height_stats_controller.rb
@@ -0,0 +1,11 @@
+module Horse
+  class HeightStatsController < ApplicationController
+    skip_after_action :verify_pundit_authorization, only: :index
+
+    def index
+      @horse = Horses::Horse.find(params[:id])
+      authorize @horse, :view_height_stats?, policy_class: CurrentStable::HorsePolicy
+    end
+  end
+end
+
