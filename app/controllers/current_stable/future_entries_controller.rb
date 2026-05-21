c05ad679c8cb0b98572d1575d1e68e83b3a9e502
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-05-21 11:25:27 +0100

    Fix future entry query sort

--- app/controllers/current_stable/future_entries_controller.rb
+++ app/controllers/current_stable/future_entries_controller.rb
@@ -3,14 +3,14 @@ module CurrentStable
     skip_after_action :verify_pundit_authorization, only: :show
 
     def show
-      @query = policy_scope(Racing::FutureRaceEntry.current_or_future.where(stable: Current.stable)).includes(:race)
+      @query = policy_scope(Racing::FutureRaceEntry.current_or_future.where(stable: Current.stable))
       @query = @query.includes(:horse, :stable, race: { track_surface: :racetrack })
       if params[:date]
         @date = Date.parse(params[:date]) if params[:date]
         @query = @query.where(race: { date: @date })
       end
       @query = @query.ransack(params[:q])
-      @query.sorts = ["status asc", "race_date asc", "race_number asc", "horse_name asc"] if @query.sorts.blank?
+      @query.sorts = ["status asc", "horse_name asc"] if @query.sorts.blank?
 
       @pagy, @results = pagy(:countless, @query.result)
       @count = @query.result.count if @pagy.page == 1
