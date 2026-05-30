f860b90f5d2d4e2bf04507411d1983bffd3c0aa7
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-05-30 11:52:16 +0100

    Dockerizing
    https://oneuptime.com/blog/post/2026-02-08-how-to-set-up-a-rails-postgresql-redis-sidekiq-stack-with-docker-compose/view

---
+++ config/initializers/gc_instrumentation.rb
@@ -0,0 +1,13 @@
+ActiveSupport::Notifications.subscribe("process_action.action_controller") do |*args|
+  event = ActiveSupport::Notifications::Event.new(*args)
+  gc_stats = GC.stat
+
+  Rails.logger.info(
+    gc_time_ms: (gc_stats[:time] || 0),
+    gc_major_count: gc_stats[:major_gc_count],
+    gc_minor_count: gc_stats[:minor_gc_count],
+    heap_slots: gc_stats[:heap_available_slots],
+    request_path: event.payload[:path],
+    duration_ms: event.duration.round(1)
+  )
+end
