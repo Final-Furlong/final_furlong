c29e3a97737c80ad41041373114722f30466c218
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-06-04 12:39:01 +0100

    Switch prod URL for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :branch, "production"
 set :stage, :production
 set :rails_env, :production
 
-set :deploy_to, "/var/www/prod.finalfurlong"
+set :deploy_to, "/var/www/www.finalfurlong"
 
 set :deploytag_time_format, "%Y.%m.%d-%H%M%S-utc"
 
