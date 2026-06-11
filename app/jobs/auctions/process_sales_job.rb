afbb9be858dce8e17bd781124b93a7275c8f546b
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-06-11 11:18:25 +0100

    Update process sales job concurrency

--- app/jobs/auctions/process_sales_job.rb
+++ app/jobs/auctions/process_sales_job.rb
@@ -5,9 +5,11 @@ class Auctions::ProcessSalesJob < ApplicationJob
 
   good_job_concurrency_rule(
     label: -> { arguments.first[:auction_id] },
-    enqueue_limit: 3,
+    total_limit: 2,
     perform_throttle: [1, 5.minutes]
   )
+
+  discard_on GoodJob::ActiveJobExtensions::Concurrency::ConcurrencyExceededError
 
   def perform(auction)
     horses = []
