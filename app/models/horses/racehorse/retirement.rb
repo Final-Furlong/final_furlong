b82be8f0d8835835241e6841dfa4593e6f8c09ce
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2026-06-29 15:40:05 +0100

    Update retirement to change horse type from racehorse

--- app/models/horses/racehorse/retirement.rb
+++ app/models/horses/racehorse/retirement.rb
@@ -16,6 +16,14 @@ module Horses::Racehorse
       horse.current_injuries.delete_all
       horse.training_schedules_horse&.destroy
       horse.breeders_cup_nomination&.destroy
+      case status
+      when "broodmare"
+        horse.update(type: "Horses::Horse::Broodmare")
+      when "stud"
+        horse.update(type: "Horses::Horse::Stud")
+      when "retired"
+        horse.update(state: "retired")
+      end
       ::Horses::Event.create!(horse:, event_type: "retired_racing", date: Date.current)
     end
   end
